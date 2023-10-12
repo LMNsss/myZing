79f7486fc4e8c596e5118c067b5d75207789665d
Admin <lmn2408ishere@gmail.com>
2023-10-13 00:25:34 +0700

    xong roi

--- app/src/main/java/com/ngoclm/myzing/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/ngoclm/myzing/ui/library/LibraryFragment.kt
@@ -8,11 +8,12 @@ import android.view.ViewGroup
 import com.google.android.material.tabs.TabLayoutMediator
 import com.ngoclm.myzing.databinding.FragmentLibraryBinding
 import com.ngoclm.myzing.ui.library.adapter.PlayListAndAlbumPagerAdapter
+import com.ngoclm.myzing.ui.library.adapter.RecentlyListAdapter
+import com.ngoclm.myzing.ui.library.playlist_and_album.AlbumFragment
+import com.ngoclm.myzing.ui.library.playlist_and_album.PlayListFragment
 
 class LibraryFragment : Fragment() {
     private lateinit var binding: FragmentLibraryBinding
-    private val tabLayout = binding.tabLayout
-    private val pager2 = binding.viewPager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +26,21 @@ class LibraryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = PlayListAndAlbumPagerAdapter(this)
-        pager2.adapter = adapter
-        TabLayoutMediator(tabLayout, pager2) {tab, pos ->
-            when(pos){
-                0 -> {tab.text = "Playlist"}
-                1 -> {tab.text = "Album"}
+        // Set up the ViewPager2 with the adapter
+        binding.viewPager.adapter = PlayListAndAlbumPagerAdapter(requireActivity())
+
+        // Connect the TabLayout and the ViewPager2
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "Playlist"
+                    binding.icMenuPlaylist.visibility = View.VISIBLE
+                }
+
+                1 -> {
+                    tab.text = "Album"
+                    binding.icMenuPlaylist.visibility = View.INVISIBLE
+                }
             }
         }.attach()
     }
--- app/src/main/java/com/ngoclm/myzing/ui/library/adapter/PlayListAndAlbumPagerAdapter.kt
+++ app/src/main/java/com/ngoclm/myzing/ui/library/adapter/PlayListAndAlbumPagerAdapter.kt
@@ -1,26 +1,28 @@
 package com.ngoclm.myzing.ui.library.adapter
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ngoclm.myzing.ui.library.LibraryFragment
 import com.ngoclm.myzing.ui.library.playlist_and_album.AlbumFragment
 import com.ngoclm.myzing.ui.library.playlist_and_album.PlayListFragment
 
-class PlayListAndAlbumPagerAdapter(fragment: Fragment) :
-    FragmentStateAdapter(fragment) {
+class PlayListAndAlbumPagerAdapter(
+    fragmentActivity: FragmentActivity
+) : FragmentStateAdapter(fragmentActivity) {
+    private val items = 2
 
     override fun getItemCount(): Int {
-        return 2
+        return items
     }
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> {
-                PlayListFragment()
-            }
-            else -> {
-                AlbumFragment()
-            }
+            0 -> PlayListFragment()
+            1 -> AlbumFragment()
+            else -> PlayListFragment()
         }
     }
 }
